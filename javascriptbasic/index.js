2f329b2eca8cd514e1c32da450ee2c8fc7d33ed9
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2024-03-10 00:27:09 +0530

    main

--- javascriptbasic/index.js
+++ javascriptbasic/index.js
@@ -1,5 +1,54 @@
 // line below is used to print on the console window
 console.log('Namaste Dunia i am samarth gupta'); 
 
-let a = 5;
-console.log(a);
+// let a = 5;
+// console.log(a);
+
+// const num = 12;
+// // num = 13;
+
+// let lastName = 'babbar';
+
+// console.log(lastName);
+
+// lastName = 3;
+// console.log(lastName);
+
+//---- ternary operator 
+
+//  let age = 27;
+//  let status = (age >= 18) ? 'yes you can vote' : 'cannot vote';
+
+//  console.log(status); 
+
+//---- if-else-if-else
+// let marks = 98;
+// if(marks >= 90){
+//     console.log('A');
+// }
+// else if(marks >= 80){
+//     console.log('B');
+// }
+// else if(marks >= 70){
+//     console.log('C');
+// }
+// else if(marks >= 60){
+//     console.log('D');
+// }
+// else{
+//     console.log('E');
+// }
+
+//----- switch case
+
+let num = 2;
+
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default : console.log('D');
+}
