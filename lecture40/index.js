720d5ed1b8704a5dd19b641cbe75ab76a89c5336
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2024-03-16 00:06:53 +0530

    main

--- lecture40/index.js
+++ lecture40/index.js
@@ -27,8 +27,8 @@ let lastName = 'Babbar';
 
 //Insertion
 
-let numbers = [1,4,5,7];
-console.log(numbers); 
+// let numbers = [1,4,5,7];
+// console.log(numbers); 
 // //end -- we use push
 // numbers.push(9);
 // console.log(numbers);
@@ -49,12 +49,13 @@ console.log(numbers);
 // console.log(numbers.indexOf(4,2));
 
 //we create an array of an object 
-let courses = [
-    {no:1, naam:'Love'},
-    {no:2, naam:'Rahul'}
-];
+// ------
+// let courses = [
+//     {no:1, naam:'Love'},
+//     {no:2, naam:'Rahul'}
+// ];
 
-console.log(courses);       
+// console.log(courses);       
 //console.log(courses.indexOf({no:1, naam:'Love'})); //iska ans -1 aa rha kyuki ye ek refernce typr hai isme address same raheta but value/reference diff
 
 //line no 61 62 63 is same as 65
@@ -62,5 +63,68 @@ console.log(courses);
 //     return course.naam == 'Rahul';    
 // });
 
-let course = courses.find(course => course.naam === 'Love');
-console.log(course);
+// let course = courses.find(course => course.naam === 'Love');
+// console.log(course);
+
+// -----Removing element
+// let numbers = [1,2,3,4,5,6];
+// //end
+// numbers.pop();
+// //begin
+// numbers.shift();
+// //middle  
+// numbers.splice(2,1);
+// console.log(numbers);
+
+//---- emptying an array
+// let numbers = [1,2,3,4,5];
+// let numbers2 = numbers;
+
+// numbers.length = 0;
+
+// console.log(numbers);
+// console.log(numbers2);
+
+// combining and slicing 
+// let first = [1,2,3];
+// let second = [4,5,6];
+
+// // let combined = first.concat(second);
+// // console.log(combined);
+
+// // let sliced = combined.slice(2,4);
+// // console.log(sliced);
+// let marks = [10,20,30,40,50,60,70,80];
+// let sliced = marks.slice(2,6);
+// console.log(sliced);
+
+//spread operator
+// let first = [1,2,3];
+// let second = [4,5,6];
+
+// let combined = [...first,...second];
+// console.log(combined);
+// // to create a copy 
+// let another = [...combined];
+// console.log(another);
+
+//iterating an array
+// let arr =[10,20,30,40,50];
+
+// for(let value of arr){
+//     console.log(value);
+// };
+
+//joining arrays
+
+// let numbers = [10,20,30,40,50];
+// const joined = numbers.join(',');
+
+// console.log(joined);
+
+//split method -- array me convert kar deta hain
+
+let message = 'This is my first message'
+let parts = message.split(' ');
+console.log(parts);
+
